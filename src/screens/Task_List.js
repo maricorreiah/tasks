d2b6790f42f4acbf341abce1116eaedc4d99bdfa
maricorreiah <mariah.pereira2@etec.sp.gov.br>
2024-05-21 10:41:28 -0300

    alteração em TaskList + npm install

--- src/screens/Task_List.js
+++ src/screens/Task_List.js
@@ -1,22 +1,40 @@
 import React, {Component} from "react" //import dos componentes
-import {StylesSheets, View, Text} from "react-native" //import do css, view e text
+import {StylesSheet, View, Text, ImageBackground} from "react-native" //import do css, view e text
+
+//referenciar a imagemm
+//../ sai da pasta
+import today_Image from "../../assets/imgs/today.jpg"
 
 // cria uma classe padrão que será usada em outras partes do programa
 export default class TaskList extends Component{ // tem todas as características de um componente (herança)
     render(){
         return(
-            <view>
-                <Text> Lista de Tarefas</Text>
+            <view style ={styles.container}>
+                <ImageBackground source={today_Image} style={styles.background}> 
+
+                </ImageBackground>
+                <view>
+
+                </view>
             </view>
         )
     }
 } 
 
 //esquema de css
-const styles = StylesSheets.create({
+const styles = StylesSheet.create({ //variável q guarda os estilos
     container: {
         flex: 1
+    },
+ //css background
+    background:{
+        flex: 3
+    },
+//css lista de tarefas
+    taskList:{
+        flex: 7
     }
+
 
 })
 
